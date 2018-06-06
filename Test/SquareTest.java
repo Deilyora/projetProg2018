9698457e1a37788bde869969d423d04637727ab4
Deilyora <deilyora@localhost.localdomain>
2018-06-06 15:17:55 +0200

    started SquareTest

---
+++ Test/SquareTest.java
@@ -0,0 +1,22 @@
+import org.junit.*;
+import static org.junit.Assert.*;
+import data.*;
+public class SquareTest{
+
+
+   @Before()
+   protected void setUp(){
+
+   }
+   @Test()
+
+   public void testAIPlay(){
+
+   }
+
+   @After()
+   protected void tearDown(){
+   
+   }
+
+}
