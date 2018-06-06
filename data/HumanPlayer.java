c0f779ee8c87127f92885abb244ffb69c305ebf9
Gilabend <gilabend.yann@gmail.com>
2018-06-06 15:52:31 +0200

    Updated method play in humanplayer (still in progress)

--- data/HumanPlayer.java
+++ data/HumanPlayer.java
@@ -1,6 +1,6 @@
 package data;
 import java.util.Scanner;
- * This class is used to represent a human player.
+ /* This class is used to represent a human player.
  * Human players choose their own name.
  */
 public class HumanPlayer extends Player {
@@ -20,9 +20,37 @@ public class HumanPlayer extends Player {
 	 * It asks the player wich pawn he wants to move, and where he wants to move it.
 	 */
 	public void play() {
+		boolean playable = false;
+		boolean existing = false;
+		int newX;
+		int newY;
+		int actualX;
+		int actualY;
+		int thePawn;
 		System.out.println(this.playerName+"'s turn :");
-		System.out.println("Enter coordinate X of the pawn you want to move");
-		Scanner in = new Scanner(System.in);
+		while (!playable) {
+			while(!valid) {
+
+				System.out.println("Enter coordinate X of the pawn you want to move");
+				Scanner in = new Scanner(System.in);
+				actualX = in.NextInt();
+				System.out.println("Enter coordinate Y of the pawn you want to move");
+				actualY = in.NextInt();
+				for (int i = 0; i < this.pawnList.length; i++) {
+					if (this.pawnList[i].getPosX() == actualX && this.pawnList[i].getPosY() == actualY && !this.pawnList[i].getEaten() && !this.pawnList[i].getFrozen() ) {
+						existing = true;
+					}
+				}
+				if (!existing) {
+					System.out.println("Error : No valide Pawn with these coordinates, try again");
+				}
+			}
+			if  (!this.pawnList[i].getEaten() && !this.pawnList[i].getFrozen() 
+			
+						
+						
+			
+		
 		
 	}
 }
