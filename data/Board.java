17b723661fd9a1226e78ca0bfa4bab4122be4b10
Deilyora <deilyora@localhost.localdomain>
2018-06-06 14:41:49 +0200

    nothing special

--- data/Board.java
+++ data/Board.java
@@ -7,7 +7,7 @@ public class Board {
 	private int height;
 	private Square[][] grid;
 	private Pawn[] frozenList;
-	
+
 	public Board(String player1, String player2, int width, int height) {
 		this.player1 = player1;
 		this.player2 = player2;
@@ -16,45 +16,45 @@ public class Board {
 		this.grid = new Square[height][width];
 		this.frozenList = new Pawn[12];
 	}
-	
+
 	private boolean movePawn(int posX, int posY) {
-		
+
 	}
-	
+
 	private boolean checkNextMove(int posX, int posY) {
-		
+
 	}
-	
+
 	public String getPlayer1() {
 		return this.player1;
 	}
-	
+
 	public String getPlayer2() {
 		return this.player2;
 	}
-	
+
 	public void setPlayer1(String newName) {
 		this.player1 = newName;
 	}
-	
+
 	public void setPlayer2(String newName) {
 		this.player2 = newName;
 	}
-	
+
 	public int getWidth() {
 		return this.width;
 	}
-	
+
 	public int getHeight() {
 		return this.Height;
 	}
-	
+
 	public void setWidth(int width) {
 		this.width = width;
 	}
 	public void setHeight(int height) {
 		this.height = height;
 	}
-	
-	
+
+
 }
