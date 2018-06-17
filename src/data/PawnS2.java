511251fb73a065842e58b87d9c0532469f0099b5
Deilyora <deilyora@localhost.localdomain>
2018-06-17 22:46:11 +0200

    Corrected javadoc errors

--- src/data/PawnS2.java
+++ src/data/PawnS2.java
@@ -1,13 +1,12 @@
 package data;
 /**
- * This class represents the size 2 pawn
- * size 2 pawns can eat size 3 pawns and can be eaten by size 1 pawns
+ * This class represents the size 2 pawn.
+ * Size 2 pawns can eat size 3 pawns and can be eaten by size 1 pawns.
  */
-
 public class PawnS2 extends Pawn {
 
 	/**
-	 *	The Pawn constructor : it inherits of the position on the grid, if it has eaten a pawn or if it is eating a pawn
+	 *	The Pawn constructor : it inherits of the position on the grid, if it has eaten a pawn or if it is eating a pawn.
 	 * @param posX The postion of the pawn on X
 	 * @param posY The postion of the pawn on Y
 	 * @param eating is true if the pawn has eaten a pawn
--- src/data/PawnS3.java
+++ src/data/PawnS3.java
@@ -1,14 +1,12 @@
 package data;
-
 /**
- * This class represents the size 3 pawn
- * size 2 pawns can eat size 4 pawns and can be eaten by size 2 pawns
+ * This class represents the size 3 pawn.
+ * Size 3 pawns can eat size 4 pawns and can be eaten by size 2 pawns.
  */
-
 public class PawnS3 extends Pawn {
 
 	/**
-	 *	The Pawn constructor : it inherits of the position on the grid, if it has eaten a pawn or if it is eating a pawn
+	 *	The Pawn constructor : it inherits of the position on the grid, if it has eaten a pawn or if it is eating a pawn.
 	 * @param posX The postion of the pawn on X
 	 * @param posY The postion of the pawn on Y
 	 * @param eating is true if the pawn has eaten a pawn
