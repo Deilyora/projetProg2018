c0520ac3ff3076eff0e254561efb7315aa25216b
Gilabend <gilabend.yann@gmail.com>
2018-06-19 16:31:17 +0200

    added javadoc

--- src/data/Mode.java
+++ src/data/Mode.java
@@ -1,5 +1,10 @@
 package data;
-
+/**
+ * This enumeration is used to differenciate the modes of the game
+ * HH = Human vs  Human
+ * HA = Human vs AI
+ * AA = AI vs AI
+ */
 public enum Mode {
 	HH,
 	HA,
