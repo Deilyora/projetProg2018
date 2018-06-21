29bc1b79a1750b3410e4c9eb695177d3f0a7c9e9
Deilyora <deilyora@localhost.localdomain>
2018-06-21 14:01:55 +0200

    started class BoardPanel

--- src/view/BoardPanel.java
+++ src/view/BoardPanel.java
@@ -1,12 +1,24 @@
 package view;
-
+import javax.swing.*;
+import java.awt.*;
 /**
  * This class creates the BoardPanel : it displays a graphic board with all the pawns.
  */
-public class BoardPanel {
+public class BoardPanel extends JPanel{
+
+	private static final int height = 7;
+	private static final int width = 8;
+	private static Color green=Color.GREEN;
 
 
 	public BoardPanel(){
+		setLayout(new GridLayout(this.height,this.width));
 
+		for(int i=0;i<height;i++){
+			for(int j=0;j<width;j++){
+				JPanel panel=new JPanel();
+				panel.setBackground(green);
+			}
+		}
 	}
 }
