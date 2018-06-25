2aef7fb4e7cf4859f7d32f271d8c924b07ab59ad
Deilyora <deilyora@localhost.localdomain>
2018-06-25 11:29:24 +0200

    added class GridButton

---
+++ src/view/GridButton.java
@@ -0,0 +1,18 @@
+package view;
+import javax.swing.*;
+
+/**
+* This class represents a button on the board.
+* It adds x and y coordinates to a button so we can know it's position on the board.
+*/
+public class GridButton extends JButton{
+
+   protected int x;
+   protected int y;
+
+   public GridButton(ImageIcon image,int x,int y){
+      super(image);
+      this.x=x;
+      this.y=y;
+   }
+}
